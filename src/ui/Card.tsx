b7ed0b8e90b149e13d54fe1924badacc7400fcdb
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2025-11-16 00:05:36 +0900

    style: 카드의 기본 디자인과 모드별 디자인을 분리 관리

--- src/ui/Card.tsx
+++ src/ui/Card.tsx
@@ -2,24 +2,42 @@ import { css } from "@emotion/css";
 import { DARKMODE, PRIMARY } from "../styles/theme";
 import useTheme from "../store/themeStore";
 
-const primaryStyle = css`
-    background-color: ${PRIMARY.surface};
-    color: ${PRIMARY.inputText};
-    border-radius: 5px;
-    border-color: ${PRIMARY.inputBorder};
-  `
-const darkStyle = css`
-    background-color: ${DARKMODE.surface};
-    color: ${DARKMODE.inputText};
-    border-radius: 5px;
-    border-color: ${DARKMODE.inputBorder};
-  `
+const baseCardStyle = css`
+  border-radius: 5px;
+  border: 1px solid;
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+  align-items: center;
+  padding: 1.5rem;
+  justify-content: space-between;
+  width: 100%;
+  box-sizing: border-box;
+`;
 
-const Card = () => {
-  const {theme} = useTheme();
+const primaryStyle = css`
+  ${baseCardStyle};
+  background-color: ${PRIMARY.surface};
+  color: ${PRIMARY.text};
+  border-color: ${PRIMARY.inputBorder};
+`;
+
+const darkStyle = css`
+  ${baseCardStyle};
+  background-color: ${DARKMODE.surface};
+  color: ${DARKMODE.text};
+  border-color: ${DARKMODE.inputBorder};
+`;
+
+interface CardProps{
+  readonly children?: React.ReactNode;
+}
+const Card = (props: CardProps) => {
+  const { theme } = useTheme();
+  const {children} = props;
   return (
     <div className={theme === 'primary' ? primaryStyle : darkStyle }>
-      
+      {children}
     </div>
   );
 };
