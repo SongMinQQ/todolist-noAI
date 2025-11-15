affac66d1cd8ac3d68a5a38c2043e47bdea2dc34
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2025-11-15 23:09:19 +0900

    refactor: input이 placeholder와 value를 조건부 props로 받을 수 있도록 수정

--- src/ui/Input.tsx
+++ src/ui/Input.tsx
@@ -3,20 +3,29 @@ import { DARKMODE, PRIMARY } from "../styles/theme";
 import useTheme from "../store/themeStore";
 
 const primaryStyle = css`
-    background-color: ${PRIMARY.inputBackground};
-    color: ${PRIMARY.inputText};
-    border-color: ${PRIMARY.inputBorder};
-  `
+  background-color: ${PRIMARY.inputBackground};
+  color: ${PRIMARY.inputText};
+  border-color: ${PRIMARY.inputBorder};
+`;
 const darkStyle = css`
-    background-color: ${DARKMODE.inputBackground};
-    color: ${DARKMODE.inputText};
-    border-color: ${DARKMODE.inputBorder};
-  `
+  background-color: ${DARKMODE.inputBackground};
+  color: ${DARKMODE.inputText};
+  border-color: ${DARKMODE.inputBorder};
+`;
 
-const Input = () => {
-  const {theme} = useTheme();
+interface InputProps {
+  placeholder?: string;
+  value?: string;
+}
+const Input = (props: InputProps) => {
+  const { theme } = useTheme();
+  const { placeholder, value } = props;
   return (
-    <input className={theme === 'primary' ? primaryStyle : darkStyle }>
+    <input
+      className={theme === 'primary' ? primaryStyle : darkStyle}
+      placeholder={placeholder}
+      value={value}
+    >
       
     </input>
   );
