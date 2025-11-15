530866b7d65017b78229686de77a10ebb892e390
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2025-11-15 23:08:31 +0900

    style: 프로젝트 레이아웃 조정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import ThemeToggleBtn from './components/ThemeToggleBtn';
+import TodoList from './components/TodoList';
 import useTheme from './store/themeStore';
 import { useEffect } from 'react';
 
@@ -10,7 +10,7 @@ function App() {
   },[theme])
   return (
     <>
-      <ThemeToggleBtn/>
+      <TodoList/>
     </>
   )
 }
