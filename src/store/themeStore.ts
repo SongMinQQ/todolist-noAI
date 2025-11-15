8c852fb1e1f46e06cc71734c3f2b16820fbc324e
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2025-11-15 19:50:36 +0900

    feat: theme store 구현

---
+++ src/store/themeStore.ts
@@ -0,0 +1,13 @@
+import { create } from "zustand";
+
+type theme = 'primary' | 'dark';
+interface themeStore {
+  theme: theme,
+  toggleTheme: (theme: theme) => void
+}
+const useTheme = create<themeStore>((set) => ({
+  theme: 'primary',
+  toggleTheme: () => set((state) => ({ theme: state.theme === 'primary' ? 'dark' : 'primary' })),
+}))
+
+export default useTheme;
